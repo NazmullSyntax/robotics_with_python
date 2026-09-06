c67696feda2582bdf603556c41ef9740fda9259c
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-09-07 00:54:04 +0600

    robot forward system

--- s.py
+++ s.py
@@ -1,8 +1,15 @@
-distance = 15
+# distance = 15
 
-if distance < 20:
-    print("Obstacle detected!")
-    print("Robot: Turn Left")
+# if distance < 20:
+#     print("Obstacle detected!")
+#     print("Robot: Turn Left")
+# else:
+#     print("Path is clear")
+#     print("Robot: Move Forward")
+
+distance = 50
+
+if distance > 30:
+    print("Robot is moving forward")
 else:
-    print("Path is clear")
-    print("Robot: Move Forward")
+    print("Robot stopped")
