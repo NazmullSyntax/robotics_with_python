71ba04ac7567b06d1175afbb5f2a7d35a347bfd5
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-09-07 00:58:14 +0600

    add battary realted

--- s.py
+++ s.py
@@ -1,46 +1,55 @@
-# # # # distance = 15
+# # # # # distance = 15
 
-# # # # if distance < 20:
-# # # #     print("Obstacle detected!")
-# # # #     print("Robot: Turn Left")
+# # # # # if distance < 20:
+# # # # #     print("Obstacle detected!")
+# # # # #     print("Robot: Turn Left")
+# # # # # else:
+# # # # #     print("Path is clear")
+# # # # #     print("Robot: Move Forward")
+
+# # # # distance = 50
+
+# # # # if distance > 30:
+# # # #     print("Robot is moving forward")
 # # # # else:
-# # # #     print("Path is clear")
-# # # #     print("Robot: Move Forward")
+# # # #     print("Robot stopped")
 
-# # # distance = 50
+# # # front = 10
+# # # left = 50
+# # # right = 30
 
-# # # if distance > 30:
-# # #     print("Robot is moving forward")
+# # # if front < 20:
+# # #     if left > right:
+# # #         print("Turn Left")
+# # #     else:
+# # #         print("Turn Right")
 # # # else:
-# # #     print("Robot stopped")
+# # #     print("Move Forward")
 
-# # front = 10
-# # left = 50
-# # right = 30
+# # light = "red"
 
-# # if front < 20:
-# #     if left > right:
-# #         print("Turn Left")
-# #     else:
-# #         print("Turn Right")
-# # else:
-# #     print("Move Forward")
+# # if light == "red":
+# #     print("Robot: STOP")
 
-# light = "red"
+# # elif light == "yellow":
+# #     print("Robot: Slow Down")
 
-# if light == "red":
-#     print("Robot: STOP")
+# # elif light == "green":
+# #     print("Robot: GO")
+# temperature = 35
 
-# elif light == "yellow":
-#     print("Robot: Slow Down")
+# if temperature > 30:
+#     print("Temperature is high")
+#     print("Robot: Turn ON Fan")
+# else:
+#     print("Temperature is normal")
+#     print("Robot: Fan OFF")
 
-# elif light == "green":
-#     print("Robot: GO")
-temperature = 35
+light = 20
 
-if temperature > 30:
-    print("Temperature is high")
-    print("Robot: Turn ON Fan")
+if light < 30:
+    print("It is dark")
+    print("Robot: Turn ON Light")
 else:
-    print("Temperature is normal")
-    print("Robot: Fan OFF")
+    print("It is bright")
+    print("Robot: Turn OFF Light")
