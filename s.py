2adfd6eb44840eb2dc7802b6724d800b3dc48612
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-09-07 00:55:57 +0600

    add trafic light robot

--- s.py
+++ s.py
@@ -1,27 +1,38 @@
-# # distance = 15
+# # # distance = 15
 
-# # if distance < 20:
-# #     print("Obstacle detected!")
-# #     print("Robot: Turn Left")
+# # # if distance < 20:
+# # #     print("Obstacle detected!")
+# # #     print("Robot: Turn Left")
+# # # else:
+# # #     print("Path is clear")
+# # #     print("Robot: Move Forward")
+
+# # distance = 50
+
+# # if distance > 30:
+# #     print("Robot is moving forward")
 # # else:
-# #     print("Path is clear")
-# #     print("Robot: Move Forward")
+# #     print("Robot stopped")
 
-# distance = 50
+# front = 10
+# left = 50
+# right = 30
 
-# if distance > 30:
-#     print("Robot is moving forward")
+# if front < 20:
+#     if left > right:
+#         print("Turn Left")
+#     else:
+#         print("Turn Right")
 # else:
-#     print("Robot stopped")
+#     print("Move Forward")
 
-front = 10
-left = 50
-right = 30
+light = "red"
 
-if front < 20:
-    if left > right:
-        print("Turn Left")
-    else:
-        print("Turn Right")
-else:
-    print("Move Forward")
+if light == "red":
+    print("Robot: STOP")
+
+elif light == "yellow":
+    print("Robot: Slow Down")
+
+elif light == "green":
+    print("Robot: GO")
